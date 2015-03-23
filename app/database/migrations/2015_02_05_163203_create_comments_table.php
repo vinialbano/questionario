ea85d922ba1b3e534c6fbe192189406609197ea8
Vinicius Albano <vinialbano@gmail.com>
2015-03-23 15:23:45 -0300

    Alteracao do tipo do comentario para text

--- app/database/migrations/2015_02_05_163203_create_comments_table.php
+++ app/database/migrations/2015_02_05_163203_create_comments_table.php
@@ -15,7 +15,7 @@ class CreateCommentsTable extends Migration {
 		Schema::create('comments', function(Blueprint $table)
 		{
 			$table->increments('id');
-			$table->string('comment');
+			$table->text('comment');
 			$table->timestamps();
 		});
 	}
