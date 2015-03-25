05018ee59e6c7f030085ed6263ba6627be07a5e9
Vinicius Albano <vinialbano@gmail.com>
2015-03-25 00:36:15 -0300

    Alteracao no texto da apresentacao

--- app/views/apresentacao.blade.php
+++ app/views/apresentacao.blade.php
@@ -15,11 +15,11 @@
                 <div class="row clearfix">
                     <div class="col-md-12 column">
                         <div class="well">
-                            <h4 class="glow in text-center">Olá, tudo bem? Seja bem vindo ao Questionário Interativo!</h4>
-                            <p class="glow in">Para responder as questões, arraste uma das opções disponíveis da área de Opções para a área de Respostas.</p>
+                            <h4 class="glow in">Olá, tudo bem? Seja bem vindo ao Questionário Interativo!</h4>
+                            <p class="glow in" style="font-weight: bold;">Para responder as questões,  basta  clicar e arrastar uma das opções disponíveis da área de Opções para a área de Respostas.</p>
                             <p class="glow in">Existem questões onde é possível adicionar uma alternativa. Para isso, um botão surgirá na área de opções e clicando nele você poderá inserir novas informações.</p>
                             <p class="glow in">Para questões com múltipla escolha, basta arrastar mais de uma opção para a área de Respostas.</p>
-                            <p class="glow in">Caso queira trocar de alternativa, basta arrastá-la de volta para a área de Opções.</p>
+                            <p class="glow in">Caso queira trocar de alternativa, se acaso errar por exemplo, basta arrastá-la de volta para a área de Opções, escolher a nova opção e arrastar novamente.</p>
                         </div>
                     </div>
                 </div>
